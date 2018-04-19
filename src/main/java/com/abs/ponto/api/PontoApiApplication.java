67c151b4e6f56bb538dfdc0951bdc1ccf57c17f6
niltonfq <niltonfq@gmail.com>
2018-04-18 22:51:40 -0300

    arquivos iniciais do projeto

---
+++ src/main/java/com/abs/ponto/api/PontoApiApplication.java
@@ -0,0 +1,12 @@
+package com.abs.ponto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoApiApplication.class, args);
+	}
+}
